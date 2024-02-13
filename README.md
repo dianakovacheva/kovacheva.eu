724d3ff246456171995df52e78b02d62828f9d9c
Diana Kovacheva <dmarinova@outlook.com>
2024-02-13 20:13:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Diana Kovacheva | Personal Portfolio Website
+
+![Diana Kovacheva | Personal Portfolio Website](www.kovacheva.eu)
+
+Hey there 👋
+I'm Diana Kovacheva
+JavaScript Web Developer
+
+## Demo
+
+View a live demo of [Diana Kovacheva | Personal Portfolio Website](https://kovacheva.eu/)
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Diana Kovacheva | JavaScript Web Developer";
+export const SITE_DESCRIPTION =
+  "Full stack JavaScript web developer ready for new challenges";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
